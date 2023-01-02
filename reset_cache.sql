05e9ed312b2de11e8fbd36c1243e3fb6e681ee7d
Danilo Vieira Costa <danilo.11235@outlook.com>
2023-01-02 00:19:41 -0300

    Registrando os importantíssimos comandos de limpeza de cache.

---
+++ reset_cache.sql
@@ -0,0 +1,5 @@
+/* Dois comandos muito úteis do SQL, especialmente para mim, são mostrados abaixo: */
+
+DBCC FREEPROCCACHE; /* limpa o cache de buffers sem reiniciar ou desligar o Servidor. */
+
+DBCC DROPCLEANBUFFERS; /* esvazia o cache de dados. */
